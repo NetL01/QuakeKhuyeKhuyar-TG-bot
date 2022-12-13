e9b48d8dbd241afdee87f2fdb3ab903195c6ce7a
Nikita Razuvaev <48290510+NetL01@users.noreply.github.com>
2022-12-14 00:29:53 +0300

    try object interanction

--- main.py
+++ main.py
@@ -1,24 +1,19 @@
 import telebot
 
-bot = telebot.TeleBot('5849840132:AAEHFN1i-u6ZiglFRYL4jcwvL-1_R9DuKdM');
-
-#@bot.message_handler(content_types=['text'])
-#def get_text_messages(message):
-#    if message.text == "кто пидор?":
-#        bot.send_message(message.from_user.id, "семен пидор")
-#    elif message.text == "кто шнурок?":
-#        bot.send_message(message.from_user.id, "кирилл шнурок")
-#    else:
-#        bot.send_message(message.from_user.id, "ты че совсем плебей ебанный?")
-
-@bot.message_handler(commands=['start'])
-def start(message):
-	bot.send_message(message.chat.id, text=f'Hello,	{message.from_user.username}!')
-
-@bot.message_handler(commands=['пидор_дня'.upper().lower()])
-def who_pidor(message):
-	bot.send_message(message.chat.id, text=f'семен пидор дня')
+try:
 
 
-bot.polling(none_stop=True, interval=0)
 
+
+	@bot.message_handler(commands=['start'])
+	def start(message):
+		bot.send_message(message.chat.id, text=f'Hello,	{message.from_user.username}!')
+
+	@bot.message_handler(commands=['пидор_дня'.upper().lower()])
+	def who_pidor(message):
+		bot.send_message(message.chat.id, text=f'семен пидор дня')
+
+
+	bot.polling(none_stop=True, interval=0)
+except:
+	print('pizdec')
